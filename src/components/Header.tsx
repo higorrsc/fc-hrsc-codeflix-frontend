7e6e14a2f253868f38d45642b450f385e79fd84f
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-08 19:40:42 -0300

    chore: modularize components and hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,21 @@
 'use client';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useScroll } from '@/hooks/useScroll';
+import { Logo } from './Logo';
+import { NavLinks } from './NavLinks';
+import { UserProfile } from './UserProfile';
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
+  const isScrolled = useScroll();
 
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  });
   return (
     <header
       className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between p-4 transition-all lg:px-10 lg:py-6`}
     >
       <div className='flex items-center space-x-2 md:space-x-8'>
-        <Image src='/logo.png' alt='Netflix' width={120} height={120} />
-        <ul className='hidden md:flex md:space-x-4'>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-        </ul>
+        <Logo />
+        <NavLinks />
       </div>
-      <div className='flex items-center space-x-4'>
-        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-        <Image
-          src='/profile.png'
-          alt='Profile'
-          width={40}
-          height={40}
-          className='cursor-pointer rounded'
-        />
-      </div>
+      <UserProfile />
     </header>
   );
 }
---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+import Image from 'next/image';
+
+export const Logo = () => {
+  return <Image src='/logo.png' alt='Netflix' width={90} height={90} />;
+};
