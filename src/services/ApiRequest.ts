7beef2f8b42ea9f660b4ce8c34461814ff8912ea
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-10 20:48:08 -0300

    chore: add api requests

---
+++ src/services/ApiRequest.ts
@@ -0,0 +1,51 @@
+const API_URL = process.env.API_URL || 'http://localhost:5000';
+
+export interface ApiQueryParams {
+  [key: string]: string | number | boolean;
+}
+
+export interface RequestOptions {
+  page?: number;
+  _limit?: number;
+  rating_like?: string;
+}
+
+export const defaultOptions: RequestOptions = {
+  page: 1,
+  _limit: 10,
+};
+
+export function buildQueryString(param: ApiQueryParams) {
+  const queryString = Object.keys(param)
+    .filter(([, value]) => value !== undefined)
+    .map(([key, value]) => [key, encodeURIComponent(String(value))]);
+
+  return `?${new URLSearchParams(Object.fromEntries(queryString)).toString()}`;
+}
+
+export async function apiRequest(
+  endpoint: string,
+  query: ApiQueryParams = {},
+  options: RequestOptions = {}
+) {
+  const mergedOptions: RequestOptions = {
+    ...defaultOptions,
+    ...options,
+  };
+
+  const queryString: string = buildQueryString({
+    ...query,
+    ...mergedOptions,
+  });
+
+  try {
+    const response = await fetch(`${API_URL}/${endpoint}${queryString}`);
+    if (!response.ok) {
+      throw new Error(`API request failed: ${response.statusText}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    throw error;
+  }
+}
