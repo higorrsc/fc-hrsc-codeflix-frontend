5686e7eff7fd9945254a3c781112cbb4e3a547a7
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-04-10 21:29:18 -0300

    chore: add get featured movie to show at banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Banner } from '@/components/Banner';
 import { Header } from '@/components/Header';
 import { MovieRow } from '@/components/MovieRow';
+import { getFeaturedMovie } from '@/services/MovieService';
 
-export default function Home() {
+export default async function Home() {
+  const featuredMovie = await getFeaturedMovie('106');
+
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b from-transparent to-black lg:h-[140vh]'>
       <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
-        <Banner />
+        <Banner movie={featuredMovie} />
         <MovieRow sectionTitle='Trending Now' />
         <MovieRow sectionTitle='Top Rated' />
         <MovieRow sectionTitle='Action Movies' />
